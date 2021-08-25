8d0471431ac239b40797f5e1d3143288a41955a9
KC <kahlen.young.daughter@gmail.com>
2021-08-25 10:32:45 -0600

    Hello World App

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        //Entry point of our program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Nico!");
+            Console.Read();
+        }
+    }
+}
