747032a36a1bf6c9fc3fdaaf47e30345cad9c299
KC <kahlen.young.daughter@gmail.com>
2021-08-30 14:08:06 -0600

    testing console and ascii values

---
+++ ASCIITest/ASCIITest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ASCIITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a string and press enter:");
+            string readInput = Console.ReadLine();
+            Console.WriteLine("you have entered {0}", readInput);
+
+            Console.Write("enter a string and press enter:");
+            int asciiValue = Console.Read();
+            Console.WriteLine("ASCII value is {0}:", asciiValue);
+            Console.ReadKey();
+        }
+    }
+}
