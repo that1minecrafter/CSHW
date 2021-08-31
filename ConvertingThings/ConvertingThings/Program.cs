bdd645c6828516cc40fe1d773ff52d1b466f1854
KC <kahlen.young.daughter@gmail.com>
2021-08-31 11:29:32 -0600

    Learning how to convert to strings, as well as implicit and explicit conversions

---
+++ ConvertingThings/ConvertingThings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConvertingThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //implicit conversion
+            int num = 12390532;
+            long bigNum = num;
+
+            float myFloat = 15.73f;
+            double myNewDouble = myFloat;
+
+            double myDouble = 15.73;
+            int myInt;
+            //explicit conversion
+            //cast double to int
+            myInt = (int)myDouble;
+
+            //type conversion
+            string myString = myDouble.ToString();
+            string myFloatString = myFloat.ToString();
+
+            Console.WriteLine(myString);
+            Console.Read();
+        }
+    }
+}
