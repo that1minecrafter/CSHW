8251d08da260f61b9b5f209783389251128e9cd4
KC <kahlen.young.daughter@gmail.com>
2021-08-31 11:29:57 -0600

    cOloRs

---
+++ ConsoleColors/ConsoleColors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleColors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
