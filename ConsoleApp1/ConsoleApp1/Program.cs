1b24bf03cfebeda75f7beeb0fab2bd0b5f6ccad5
KC <kahlen.young.daughter@gmail.com>
2021-09-02 12:24:17 -0600

    more strings...

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the string here: ");
+            string input = Console.ReadLine();
+
+            Console.Write("enter a character to search: ");
+            char searchInput = Console.ReadLine()[0];
+            int searchIndex = input.IndexOf(searchInput);
+
+            Console.WriteLine("index of character {0} in string is {1}", searchInput, searchIndex);
+
+            Console.Write("enter first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("enter last name: ");
+            string lastName = Console.ReadLine();
+
+            string fullName = string.Concat(firstName, " ", lastName);
+            Console.WriteLine("your full name {0}", fullName);
+
+            Console.ReadKey();
+        }
+    }
+}
