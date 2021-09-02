295eded9ab0d75e0434884207e82704ed8861080
KC <kahlen.young.daughter@gmail.com>
2021-09-02 12:23:55 -0600

    printing name different ways

---
+++ StringChallenge/StringChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            Console.Write("please enter your name and press enter: ");
+            myName = Console.ReadLine();
+            string myNameUpper = String.Format("Uppercase: {0}", myName.ToUpper());
+            string myNameLower = String.Format("Lowercase: {0}", myName.ToLower());
+            string myNameTrimmed = String.Format("Trimmed Value: {0}", myName.Trim());
+            string myNameSubString = String.Format("Substring Value: {0}", myName.Substring(0, 4));
+
+            Console.WriteLine(myNameUpper);
+            Console.WriteLine(myNameLower);
+            Console.WriteLine(myNameTrimmed);
+            Console.WriteLine(myNameSubString);
+
+            Console.ReadKey();
+        }
+    }
+}
