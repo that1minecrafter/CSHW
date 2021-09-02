003ccbc2caf7e6692961a044df3439c5d6db39a2
KC <kahlen.young.daughter@gmail.com>
2021-09-02 11:10:25 -0600

    working on more strings

---
+++ StringsNStuff/StringsNStuff/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringsNStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 14;
+            string name = "Nico";
+            string grade = "sophmore";
+
+            //string concatentation
+            Console.WriteLine("Hello my name is " + name + ", I am " + age + " years old, and i am a " + grade);
+
+            //string formatting
+            Console.WriteLine("hello my name is {0}, I am {1} years old, and i am a {2}", name, age, grade);
+
+
+        }
+    }
+}
