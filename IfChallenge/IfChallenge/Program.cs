53a8bfb9996cb4ff4090f2950163c40f241c3e35
KC <kahlen.young.daughter@gmail.com>
2021-10-05 13:44:55 -0600

    IfElse pt2

---
+++ IfChallenge/IfChallenge/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace IfChallenge
+{
+    class Program
+    {
+        static string username;
+        static string password;
+
+        static void Main(string[] args)
+        {
+            Register();
+            Login();
+            Console.Read();
+        }
+
+        public static void Register()
+        {
+            Console.WriteLine("please enter your username");
+            username = Console.ReadLine();
+            Console.WriteLine("please enter your password");
+            password = Console.ReadLine();
+            Console.WriteLine("user registered!");
+            Console.WriteLine("---------------------------");
+        }
+
+        public static void Login()
+        {
+            Console.WriteLine("please enter your username");
+            if(username == Console.ReadLine())
+            {
+                Console.WriteLine("please enter your password");
+                if(password == Console.ReadLine())
+                {
+                    Console.WriteLine("Login successful!");
+                }
+                else
+                {
+                    Console.WriteLine("login failed, please restart program!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("login failed, please restart program!");
+            }
+        }
+    }
+}
