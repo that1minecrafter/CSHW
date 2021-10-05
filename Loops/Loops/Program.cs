6a6318078679dad833a34c05a66bc597c01c3cf2
KC <kahlen.young.daughter@gmail.com>
2021-10-05 13:44:23 -0600

    ForLoops

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int counter = 1; counter < 20; counter+=2)
+            {
+                Console.WriteLine(counter);
+            }
+            Console.WriteLine("for loop is done");
+        }
+    }
+}
