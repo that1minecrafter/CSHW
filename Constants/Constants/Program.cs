b2f8876e25e99ce284a529d715cd4169b7321560
KC <kahlen.young.daughter@gmail.com>
2021-10-05 13:45:57 -0600

    Constants

---
+++ Constants/Constants/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string birthday = "12/21/2006";
+
+            Console.WriteLine("My birthday is always going to me: {0}", birthday);
+            Console.ReadKey();
+        }
+    }
+}
