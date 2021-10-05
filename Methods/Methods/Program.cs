7142c31c2514badc0858cb66d2d3a074f11c6473
KC <kahlen.young.daughter@gmail.com>
2021-10-05 13:43:42 -0600

    Methods pt2

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+            WriteSomethingSpecific("i am an argument called from a method");
+            Console.Read();
+        }
+
+        public static void  WriteSomething()
+        {
+            Console.WriteLine("i am called from a method");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+    }
+}
