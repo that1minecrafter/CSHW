8f4c8c2033a2a90706c7b68648c47e122ac00825
KC <kahlen.young.daughter@gmail.com>
2021-10-05 13:43:27 -0600

    Methods pt1

---
+++ methodsWithReturns/methodsWithReturns/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace methodsWithReturns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(15,31));
+            Console.WriteLine(Multiply(9,12));
+            Console.WriteLine(Divide(25, 13));
+            Console.Read();
+
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+}
