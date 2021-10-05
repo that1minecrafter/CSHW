464e6fc0e5cd603a8f54cc47613da25904815ce8
KC <kahlen.young.daughter@gmail.com>
2021-10-05 13:43:06 -0600

    NestedIfStatements

---
+++ NestedIf/NestedIf/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NestedIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+            Console.WriteLine("please enter your username");
+
+
+
+            userName = Console.ReadLine();
+            if (isRegistered && userName != "" && userName.Equals("Admin"))
+            {
+                Console.WriteLine("hi there, registered user!");
+
+                Console.WriteLine("hi there, " + userName);
+
+                Console.WriteLine("hi there, Admin!");
+
+            }
+
+            if(isAdmin || isRegistered)
+            {
+                Console.WriteLine("you are logged in");
+            }
+            Console.Read();
+        }
+    }
+}
