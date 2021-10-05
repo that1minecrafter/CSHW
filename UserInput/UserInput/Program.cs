062e4bb11d8aa95f04b4537b724758d7bb0ea579
KC <kahlen.young.daughter@gmail.com>
2021-10-05 13:40:37 -0600

    Simple Adding

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate());
+            Console.Read();
+        }
+
+        public static int Calculate()
+        {
+            Console.WriteLine("please enter the first number");
+            string number1 = Console.ReadLine();
+            Console.WriteLine("please enter second number");
+            string number2 = Console.ReadLine();
+
+            int num1 = int.Parse(number1);
+            int num2 = int.Parse(number2);
+
+            int result = num1 + num2;
+            return result;
+
+        }
+    }
+}
