25468324f4c005608a028c21da464ed29178fb8d
KC <kahlen.young.daughter@gmail.com>
2021-10-05 13:44:04 -0600

    methods pt3

---
+++ MethodChallenge/MethodChallenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Micky";
+            string name2 = "Alex";
+            string name3 = "Shaymus";
+            GreetFriend(name1);
+            GreetFriend(name2);
+            GreetFriend(name3);
+        }
+        static public void GreetFriend(string name)
+        {
+            Console.WriteLine("haii " + name);
+        }
+    }
+}
