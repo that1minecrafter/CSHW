22a7d5f62ce7f4f924ed15aae61671e6507da6e3
KC <kahlen.young.daughter@gmail.com>
2021-10-05 13:45:47 -0600

    CompactIfElse

---
+++ EnhancedIfStatements/EnhancedIfStatements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnhancedIfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 96;
+            string stateOfMatter;
+
+            /*
+            if (temp < 32)
+                stateOfMatter = "solid";
+            else
+                stateOfMatter = "liquid";
+            */
+            stateOfMatter = temp < 32 ? "solid" : "liquid" ;
+
+            stateOfMatter = temp > 212 ? "gas" : temp < 0 ? "solid" : "liquid"; 
+
+            Console.WriteLine("state of matter is " + stateOfMatter);
+            Console.ReadKey();
+        }
+    }
+}
