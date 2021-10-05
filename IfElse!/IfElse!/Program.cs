7ef5a49db92501527dcb43cf4c343b890adfe0f6
KC <kahlen.young.daughter@gmail.com>
2021-10-05 13:44:37 -0600

    IfElse

---
+++ IfElse!/IfElse!/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IfElse_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what's the temperature like?");
+            string temp = Console.ReadLine();
+            int numTemp = int.Parse(temp);
+
+            if(numTemp < 40)
+            {
+                Console.WriteLine("take a jacket");
+            }else if(numTemp == 60)
+            {
+                Console.WriteLine("it's a nice temperature outside!");
+            }else if(numTemp > 70)
+            {
+                Console.WriteLine("it's too hot for this");
+            }
+
+            Console.Read();
+        }
+    }
+}
