90535bdd6d1e997a63b8e808d1c3c2b3d03d3e84
KC <kahlen.young.daughter@gmail.com>
2021-10-05 13:41:31 -0600

    Ternary Op With Temp

---
+++ TernaryOperatorChallenge/TernaryOperatorChallenge/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TernaryOperatorChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputTemp = 0;
+            string tempMsg = string.Empty;
+            string inputValue = string.Empty;
+
+            Console.Write("enter the current temperature");
+            inputValue = Console.ReadLine();
+
+            bool validint = int.TryParse(inputValue, out inputTemp);
+
+            if(validint)
+            {
+                tempMsg = inputTemp <= 15 ? "it's cold here" : (inputTemp >= 16 && inputTemp <= 70) ? "it's okay" : inputTemp > 70 ? "it's hot here" : "";
+                Console.WriteLine(tempMsg);
+            }
+            else
+            {
+                Console.WriteLine("not a valid temperature!");
+            }
+        }
+    }
+}
